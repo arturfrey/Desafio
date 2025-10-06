aad0fe4accf468644b258b435526e39309d2aeb5
arturfrey <arturfrey3@gmail.com>
2025-10-06 15:39:06 -0300

    Desafio 5

---
+++ src/FizzBuzz.java
@@ -0,0 +1,22 @@
+     public class FizzBuzz {
+
+        void executar(int limite) {
+            for (int i = 1; i <= limite; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println("FizzBuzz");
+                } else if (i % 3 == 0) {
+                    System.out.println("Fizz");
+                } else if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                } else {
+                    System.out.println(i);
+                }
+            }
+        }
+
+        public static void main(String[] args) {
+            FizzBuzz fb = new FizzBuzz();
+            fb.executar(15);
+        }
+    }
+
